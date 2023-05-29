ea430b8585b1abb2d2373a450776568a288e46bd
chengsongren <2360737675@qq.com>
2023-05-29 20:17:47 +0800

    开发接口调用的SDK

--- src/main/java/com/tobuv/project/MyApplication.java
+++ src/main/java/com/tobuv/project/MyApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.PriorityQueue;
+
 @SpringBootApplication
 @MapperScan("com.tobuv.project.mapper")
 public class MyApplication {
